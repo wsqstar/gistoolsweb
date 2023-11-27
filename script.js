418c381a40fe4b481336cfa99760d7b3d23c0bd4
Shiqi <wsq_star@outlook.com>
2023-11-27 23:05:48 +0800

    增加测试代码

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function openTool(toolName) {
+    console.log('openTool function called with toolName:', toolName);
+
     var tools = document.getElementsByClassName('tool');
     for (var i = 0; i < tools.length; i++) {
         tools[i].style.display = 'none';
     }
 
     if (toolName === 'tool1') {
-        // 如果是工具1，直接跳转到 antlv7
+        console.log('Redirecting to https://l7editor.antv.antgroup.com');
         window.location.href = 'https://l7editor.antv.antgroup.com';
     } else {
-        // 否则显示选定的工具内容
         var selectedTool = document.getElementById(toolName);
         if (selectedTool) {
             selectedTool.style.display = 'block';
