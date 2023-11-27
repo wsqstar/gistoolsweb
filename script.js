8963284dc70cb26876633d56d6450afc84897984
Shiqi <wsq_star@outlook.com>
2023-11-27 22:56:07 +0800

    ver3

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function openTool(toolName) {
-    // 隐藏所有工具
     var tools = document.getElementsByClassName('tool');
     for (var i = 0; i < tools.length; i++) {
         tools[i].style.display = 'none';
     }
 
-    // 显示选定的工具
     var selectedTool = document.getElementById(toolName);
     if (selectedTool) {
         selectedTool.style.display = 'block';
